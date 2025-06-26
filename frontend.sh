d14c8616f64edda5d9bb382abfe83a1b75bf7e8f
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-25 20:56:12 -0400

    some message

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 
-userid=$(id -u)
-
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-       echo -e "$2..$R is failure $N"
-     else
-        echo -e "$2..$G is Success $N"
-    fi
-}
-
-if [ $userid -ne 0 ]
-then 
-    echo "you're not a superuser"
-    exit 1
-else
-    echo "youre a superuser"
-fi
+source ./common.sh
+check_root
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "installing nginx"
@@ -47,7 +22,7 @@ cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "unzip the frontend document"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell-1/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "copied backend expense conf"
 
 systemctl restart nginx &>>$LOGFILE
