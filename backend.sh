e5b78d6f966cbb1c3d39142a19d7d3824ce7af92
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-25 20:49:12 -0400

    some message

--- backend.sh
+++ backend.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-userid=$(id -u)
-
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-# echo "Enter the Password"
-# read mysql_root_Password
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2..$R is Failure $N"
-    else
-        echo -e "$2..$G is Success $N"
-    fi
-}
-
-if [ $? -ne 0 ]
-then 
-    echo "You're not a Superuser"
-    exit 1
-else    
-    echo "You're a Superuser"
-fi
+source ./common.sh
+check_root
 
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "disable nodejs lower version"
